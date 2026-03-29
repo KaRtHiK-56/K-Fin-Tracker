7d6ff454346d2e01661deac36e2759d0b389a927
Karthik <karthiksurya611@gmail.com>
2026-03-29 13:51:54 +0530

    Update Layout.tsx

--- K-Fin-Tracker/src/components/layout/Layout.tsx
+++ K-Fin-Tracker/src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { signOut } from '../../lib/supabase'
-import { useTheme } from '../../lib/ThemeContext'
+import { useTheme } from '../lib/ThemeContext'
 
 interface Props {
   children: React.ReactNode
